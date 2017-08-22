ec7721752ec91dc7e598c5313de55c60483736d6
Justin Li <justinnhli@gmail.com>
2017-08-22 12:14:58 -0700

    give empty extension to sed -i

--- dump.sh
+++ dump.sh
@@ -2,6 +2,6 @@
 
 sqlite3 counts.db .schema > schema.sql
 sqlite3 counts.db .dump > data.sql
-sed -i '1s/foreign_keys=OFF/foreign_keys=ON/' data.sql
+sed -i '' '1s/foreign_keys=OFF/foreign_keys=ON/' data.sql
 rm -f counts.db
 sqlite3 counts.db '.read data.sql'
