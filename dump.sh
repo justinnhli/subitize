f9e73ffefa7b1dcd97ad147d17b590d70ce28a4a
Justin Li <justinnhli@gmail.com>
2017-08-27 09:40:27 -0700

    robustify dump.sh to different unixes

--- dump.sh
+++ dump.sh
@@ -2,6 +2,11 @@
 
 sqlite3 counts.db .schema > schema.sql
 sqlite3 counts.db .dump > data.sql
-sed -i '' '1s/foreign_keys=OFF/foreign_keys=ON/' data.sql
+case "$(uname)" in
+"Linux")
+	sed -i '1s/foreign_keys=OFF/foreign_keys=ON/' data.sql;;
+"Darwin")
+	sed -i '' '1s/foreign_keys=OFF/foreign_keys=ON/' data.sql;;
+esac
 rm -f counts.db
 sqlite3 counts.db '.read data.sql'
