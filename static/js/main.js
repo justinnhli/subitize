4ac2683a826ba045ca365849feeae8257ec67d12
Justin Li <justinnhli@gmail.com>
2018-03-10 15:56:34 -0800

    refactor event handlers into separate functions

--- static/js/main.js
+++ static/js/main.js
@@ -2,56 +2,66 @@
 
 $(function () {
 
+    function searchbar_focus_handler() {
+        var searchbar = $(this);
+        if (searchbar.val() === "search for courses...") {
+            searchbar.val("");
+            searchbar.css("color", "#000000");
+        }
+    }
+
+    function searchbar_blur_handler() {
+        var searchbar = $("#searchbar");
+        if (searchbar.val() === "") {
+            searchbar.val("search for courses...");
+            searchbar.css("color", "#BABDB6");
+        }
+    }
+
+    function advanced_toggle_click_handler() {
+        var toggle = $(this);
+        var state = $("#advanced-state");
+        var div = $("#advanced-search");
+        if (state.val().toLowerCase() === "true") {
+            div.css("display", "none");
+            toggle.html("Show Options");
+            state.val("false");
+        } else {
+            div.css("display", "block");
+            toggle.html("Hide Options");
+            state.val("true");
+        }
+    }
+
+    function more_info_click_handler() {
+        var more_info = $(this);
+        var desc_tr = more_info.parents("tbody").children(".description");
+        var colspan = desc_tr.children(".description").attr("colspan");
+        var tds = more_info.parents("td").prev("td").nextAll("td").slice(0, colspan);
+        $.each(tds, function (i, td) {
+            td = $(td);
+            td.width(td.width());
+        });
+        desc_tr.toggle();
+        if (desc_tr.is(":visible")) {
+            more_info.html("[-]");
+        } else {
+            more_info.html("[+]");
+        }
+    }
+
     function main() {
+        var searchbar = $("#searchbar");
+        searchbar.focus(searchbar_focus_handler);
+        searchbar.blur(searchbar_blur_handler);
 
-        $("#searchbar").focus(function () {
-            var searchbar = $(this);
-            if (searchbar.val() === "search for courses...") {
-                searchbar.val("");
-                searchbar.css("color", "#000000");
-            }
-        }).blur(function () {
-            var searchbar = $("#searchbar");
-            if (searchbar.val() === "") {
-                searchbar.val("search for courses...");
-                searchbar.css("color", "#BABDB6");
-            }
-        });
+        var advanced_toggle = $("#advanced-toggle");
+        advanced_toggle.click(advanced_toggle_click_handler);
 
-        $("#advanced-toggle").click(function () {
-            var toggle = $(this);
-            var state = $("#advanced-state");
-            var div = $("#advanced-search");
-            if (state.val().toLowerCase() === "true") {
-                div.css("display", "none");
-                toggle.html("Show Options");
-                state.val("false");
-            } else {
-                div.css("display", "block");
-                toggle.html("Hide Options");
-                state.val("true");
-            }
-        });
-
-        $(".more-info").click(function () {
-            var more_info = $(this);
-            var desc_tr = more_info.parents("tbody").children(".description");
-            var colspan = desc_tr.children(".description").attr("colspan");
-            var tds = more_info.parents("td").prev("td").nextAll("td").slice(0, colspan);
-            $.each(tds, function (i, td) {
-                td = $(td);
-                td.width(td.width());
-            });
-            desc_tr.toggle();
-            if (desc_tr.is(":visible")) {
-                more_info.html("[-]");
-            } else {
-                more_info.html("[+]");
-            }
-        });
+        var more_info = $(".more-info");
+        more_info.click(more_info_click_handler);
 
         $("#advanced-toggle").click().click();
-
     }
 
     main();
