37d3bf88d4714715f47ae74b72ef182649e2732b
Justin Li <justinnhli@gmail.com>
2018-04-13 14:44:21 -0700

    fix and refactor scripts

--- scripts/dump.sh
+++ scripts/dump.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
-SCHEMA_SQL=subitize/data/schema.sql
-DATA_SQL=subitize/data/data.sql
-COUNTS_DB=subitize/data/counts.db
-LAST_UPDATE=subitize/data/last-update
-if [ ! -e "$COUNTS_DB" ]; then
+schema_sql=subitize/data/schema.sql
+data_sql=subitize/data/data.sql
+counts_db=subitize/data/counts.db
+last_update=subitize/data/last-update
+
+if [ ! -e "$counts_db" ]; then
 	exit
 fi
-sqlite3 "$COUNTS_DB" .schema > "$SCHEMA_SQL"
-sqlite3 "$COUNTS_DB" .dump > "$DATA_SQL"
+sqlite3 "$counts_db" .schema > "$schema_sql"
+sqlite3 "$counts_db" .dump > "$data_sql"
 case "$(uname)" in
 "Linux")
-	sed -i '1s/foreign_keys=OFF/foreign_keys=ON/' "$DATA_SQL";;
+	sed -i '1s/foreign_keys=OFF/foreign_keys=ON/' "$data_sql";;
 "Darwin")
-	sed -i '' '1s/foreign_keys=OFF/foreign_keys=ON/' "$DATA_SQL";;
+	sed -i '' '1s/foreign_keys=OFF/foreign_keys=ON/' "$data_sql";;
 esac
-rm -f "$COUNTS_DB"
-sqlite3 "$COUNTS_DB" '.read data/data.sql'
+rm -f "$counts_db"
+sqlite3 "$counts_db" ".read $data_sql"
 if git status | grep modified >/dev/null; then
-	date '+%Y-%m-%d %H:%M:%S %Z' > "$LAST_UPDATE"
+	date '+%Y-%m-%d %H:%M:%S %Z' > "$last_update"
 fi
