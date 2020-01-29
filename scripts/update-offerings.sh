c55f2a8fb01ad4739a50f74a1443bee76b1cbcce
Justin Li <justinnhli@gmail.com>
2020-01-29 10:34:27 -0800

    remove bashisms

--- scripts/update-offerings.sh
+++ scripts/update-offerings.sh
@@ -2,10 +2,9 @@
 
 counts_db=subitize/data/counts.db
 
-source "$HOME/.bashrc" && \
-    source "$HOME/.dot_secrets/bashrc" && \
-    source $PYTHON_VENV_HOME/subitize/bin/activate && \
-    cd ~/git/subitize/ && \
+. "$HOME/.bashrc" && \
+    . "$PYTHON_VENV_HOME/subitize/bin/activate" && \
+    cd "$HOME/git/subitize/" && \
     rm -f "$counts_db" && \
     ./scripts/update-offerings.py && \
     ./scripts/dump.sh && \
