671a989dbeb5cac56cb6c619775d5a50ed1528fb
Justin Li <justinnhli@gmail.com>
2023-10-28 10:10:25 -0700

    make update changes printout more informative

--- scripts/update.py
+++ scripts/update.py
@@ -301,20 +301,18 @@ def create_objects(
             num_reserved_open=0,
             num_waitlisted=0,
         )
+        print(f'adding {offering}: {offering.title} -> {title}')
     else:
         offering = offering.first()
         if offering.title != title:
-            print(' '.join([
-                f'title change in {offering.semester.code}',
-                f'offering {offering.course.department.code} {offering.course.number} {offering.section}:',
-                f'{offering.title} -> {title}',
-            ]))
-        if offering.units != int(units):
-            print(' '.join([
-                'units change in {offering.semester.code}',
-                f'offering {offering.course.department.code} {offering.course.number} {offering.section}:',
-                f'{offering.units} -> {units}',
-            ]))
+            print(f'changing title of {offering}: {offering.title} -> {title}')
+        if set(offering.instructors) != set(instructors):
+            if offering.instructors:
+                old_instructors = ', '.join(sorted(str(instructor) for instructor in offering.instructors))
+            else:
+                old_instructors = '(none)'
+            new_instructors = ', '.join(sorted(str(instructor) for instructor in instructors))
+            print(f'changing instructors of {offering}: {old_instructors} -> {new_instructors}')
     offering.title = title
     offering.units = int(units)
     offering.instructors = instructors
@@ -444,7 +442,7 @@ def delete_section(session, semester_code, dept, num, sec):
     statement = filter_by_number_str(statement, num)
     statement = filter_by_section(statement, sec)
     for offering in session.scalars(statement):
-        print(f'deleting {semester_code} offering of {dept} {num} {sec}')
+        print(f'deleting {offering}: {offering.title}')
         session.delete(offering)
 
 
