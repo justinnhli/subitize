dcd37d7c8abb9c2c8dc7b0ed77b2163ad9be7867
Justin Li <justinnhli@gmail.com>
2023-10-28 09:09:07 -0700

    use SQLAlchemy 2 for tests

--- tests/subitize_test.py
+++ tests/subitize_test.py
@@ -4,81 +4,95 @@
 
 import sys
 from os.path import dirname, realpath, join as join_path
+from pathlib import Path
 
-sys.path.append(join_path(dirname(realpath(__file__)), '..'))
+from sqlalchemy.sql.expression import func
 
-from subitize import create_session, create_query
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from subitize import create_session, create_select
 from subitize import filter_by_semester, filter_by_department, filter_by_number, filter_by_instructor
 from subitize import filter_by_units, filter_by_core, filter_by_meeting
 
 def test_semester_query():
-    session = create_session()
-    query = create_query(session)
-    query = filter_by_semester(session, query, 201701)
-    assert all(offering.semester_id == 201701 for offering in query)
-    assert query.count() == 816
+    query = create_select()
+    query = filter_by_semester(query, 201701)
+    with create_session() as session:
+        assert all(offering.semester_id == 201701 for offering in session.scalars(query))
+        assert len(list(session.scalars(query))) == 816
 
 
 def test_department_query():
-    session = create_session()
-    query = create_query(session)
-    query = filter_by_semester(session, query, 201701)
-    query = filter_by_department(session, query, 'COGS')
-    assert all(offering.course.department.code == 'COGS' for offering in query)
-    assert query.count() == 15
+    query = create_select()
+    query = filter_by_semester(query, 201701)
+    query = filter_by_department(query, 'COGS')
+    with create_session() as session:
+        assert all(offering.course.department.code == 'COGS' for offering in session.scalars(query))
+        assert len(list(session.scalars(query))) == 15
 
 
 def test_number_query():
-    session = create_session()
-    query = create_query(session)
-    query = filter_by_semester(session, query, 201701)
-    query = filter_by_department(session, query, 'COGS')
-    query = filter_by_number(session, query, minimum=200, maximum=300)
-    assert all(200 <= offering.course.number_int <= 300 for offering in query)
-    assert query.count() == 8
+    query = create_select()
+    query = filter_by_semester(query, 201701)
+    query = filter_by_department(query, 'COGS')
+    query = filter_by_number(query, minimum=200, maximum=300)
+    with create_session() as session:
+        assert all(200 <= offering.course.number_int <= 300 for offering in session.scalars(query))
+        assert len(list(session.scalars(query))) == 8
 
 
 def test_unit_query():
-    session = create_session()
-    query = create_query(session)
-    query = filter_by_semester(session, query, 201701)
-    query = filter_by_department(session, query, 'COGS')
-    query = filter_by_units(session, query, 2)
-    assert all(offering.units == 2 for offering in query)
-    assert query.count() == 2
+    query = create_select()
+    query = filter_by_semester(query, 201701)
+    query = filter_by_department(query, 'COGS')
+    query = filter_by_units(query, 2)
+    with create_session() as session:
+        assert all(offering.units == 2 for offering in session.scalars(query))
+        assert len(list(session.scalars(query))) == 2
 
 
 def test_instructor_query():
-    session = create_session()
-    query = create_query(session)
-    query = filter_by_semester(session, query, 201701)
-    query = filter_by_instructor(session, query, 'Justin Li')
-    assert query.count() == 4
+    query = create_select()
+    query = filter_by_semester(query, 201701)
+    query = filter_by_instructor(query, 'Justin Li')
+    with create_session() as session:
+        assert len(list(session.scalars(query))) == 4
 
 
 def test_core_query():
-    session = create_session()
-    query = create_query(session)
-    query = filter_by_semester(session, query, 201701)
-    query = filter_by_core(session, query, 'CPFA')
-    query = filter_by_core(session, query, 'CPGC')
-    query = filter_by_core(session, query, 'CPPE')
-    assert query.count() == 1
+    query = create_select()
+    query = filter_by_semester(query, 201701)
+    query = filter_by_core(query, 'CPFA')
+    query = filter_by_core(query, 'CPGC')
+    query = filter_by_core(query, 'CPPE')
+    with create_session() as session:
+        assert len(list(session.scalars(query))) == 1
 
 
 def test_meeting_query_normal():
-    session = create_session()
-    query = create_query(session)
-    query = filter_by_semester(session, query, 201701)
-    query = filter_by_department(session, query, 'COGS')
-    query = filter_by_meeting(session, query, days='T')
-    assert query.count() == 6
+    query = create_select()
+    query = filter_by_semester(query, 201701)
+    query = filter_by_department(query, 'COGS')
+    query = filter_by_meeting(query, days='T')
+    with create_session() as session:
+        assert len(list(session.scalars(query))) == 6
 
 
 def test_meeting_query_tbd():
-    session = create_session()
-    query = create_query(session)
-    query = filter_by_semester(session, query, 201701)
-    query = filter_by_department(session, query, 'COGS')
-    query = filter_by_meeting(session, query, days='MTWRFU')
-    assert query.count() == 2
+    query = create_select()
+    query = filter_by_semester(query, 201701)
+    query = filter_by_department(query, 'COGS')
+    query = filter_by_meeting(query, days='MTWRFU')
+    with create_session() as session:
+        assert len(list(session.scalars(query))) == 2
+
+
+if __name__ == '__main__':
+    test_semester_query()
+    test_department_query()
+    test_number_query()
+    test_unit_query()
+    test_instructor_query()
+    test_core_query()
+    test_meeting_query_normal()
+    test_meeting_query_tbd()
